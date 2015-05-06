88d5225b51418168a98e1292c3edb3962891a607
DeltaCS13 <DeltaCS@gmx.com>
2015-05-06 16:04:58 -0400

    Final Final Commit Realy, No Realy!

--- views/regAddress.php
+++ views/regAddress.php
@@ -31,7 +31,7 @@
 
 			<fieldset><h1>New Member Registration</h1>
 						<h3>Add Address</h3></fieldset>
-				<p>Fields marked with an asterisks (*) are requiered.</p>
+				<p>Fields marked with an asterisks (<span class="redText">*</span>) are requiered.</p>
 
 
 			<input type="hidden" name="action" value="registerAddress">
@@ -39,7 +39,7 @@
 			email<span class="requiered">*</span>:<br>
 				<input type="text" name="email" placeholder="example@email.com" required value="<?php if(isset($_POST['email'])) echo $_POST['email'];?>"><br>
 
-			Address Type:<br>
+			Address Typespan<span class="redText">*</span>:<br>
 				<select name="type" required>
 					<option value="">Please Select</option>
 					<option value="home">Home</option>
@@ -48,7 +48,7 @@
 				</select>
 			<br>
 
-			Address<span class="requiered">*</span>:<br>
+			Address<span class="redText">*</span>:<br>
 				<input type="text" name="address1" placeholder="555 Example St" required value="<?php if(isset($_POST['address1']))echo $_POST['address1'];?>"><br>
 
 			Address 2:<br>
@@ -57,10 +57,10 @@
 			Address 3:<br>
 				<input type="text" name="address3" placeholder="optional" value="<?php if(isset($_POST['address3'])) echo $_POST['address3'];?>"><br>
 			
-			City<span class="requiered">*</span>:<br>
+			City<span class="redText">*</span>:<br>
 				<input type="text" name="city" placeholder="Asheville" required value="<?php if(isset($_POST['city'])) echo $_POST['city'];?>"><br>
 
-			State/Provence<span class="requiered">*</span>:<br>
+			State/Provence<span class="redText">*</span>:<br>
 				<select name="region" required>
 					<option value="">Please Select</option>
 					<?php $regions = allRegions();
@@ -82,7 +82,7 @@
 					</select>
 				<br>
 
-			Postal Code/ Zip Code:<br>
+			Postal Code/ Zip Code<span span class="redText">*</span>:<br>
 				<input type="text" name="zipCode" placeholder="00000-0000" required value="<?php if(isset($_POST['zipCode'])) echo $_POST['zipCode'];?>"><br>
 
 				<input type="submit" value="Continue" />
